0dbc616b7de11d651be5fd08a83727b54d9d2c50
yudong <yoshino1347716570@gmail.com>
2019-10-01 06:32:30 +0800

    That is why it is important to have lunch! I have to redo this

---
+++ Python Fundamentals/For_Loop_Basic_I.py
@@ -0,0 +1,36 @@
+#Basic
+for i in range(0,151):
+    print(i)
+
+#Multiples of Five
+for i in range(5,1001,5):
+    print(i)
+
+#Counting, the Dojo Way
+for i in range(1,101,1):
+    if i%10==0:
+        print("Coding Dojo")
+    elif i%5==0:
+        print("Coding")
+    else:
+        print(i)
+
+#Whoa. That Sucker's Huge
+sum = 0
+for i in range(0,500000,2):
+    sum+=i
+print(sum)
+
+#Countdown by Fours
+for i in range(2018,0,-4):
+    print(i)
+
+print("-------")
+#Flexible Counter
+def flexible_counter(lowNum,highNum,mult):
+    same_line=""
+    for i in range(lowNum,highNum+1):
+        if i%mult==0:
+            same_line+=str(i)+", "
+    return same_line.strip(', ')
+print(flexible_counter(2,9,3))
