3a1baeb442ff2bc799c2cfabbee765f9ef0e0123
yudong <yoshino1347716570@gmail.com>
2019-10-04 06:54:30 +0800

    done

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +19,20 @@ def say_many_time(times,say_what):
     for i in range(times):
         say_return += say_what +" "
     return say_return
+
+@app.route('/play')
+def index_default():
+   return render_template("index.html", number_of_box=3, color_box="blue")
+
+@app.route('/play/<int:x>')
+def index_default_2(x):
+   return render_template("index.html", number_of_box=x, color_box="blue")
+
+@app.route('/play/<int:x>/<color>')
+def index_default_3(x,color):
+   return render_template("index.html", number_of_box=x, color_box=color)
+
+
 @app.route('/<others>')
 def warning_system(others):
     return "Sorry! No response. Try again."
