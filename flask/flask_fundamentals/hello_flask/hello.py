150824ac7851e43fd559a0c62606e980cfb687d5
yudong <yoshino1347716570@gmail.com>
2019-10-04 06:00:47 +0800

    Update hello.py

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -3,7 +3,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+   return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+   return 'Dojo'
+
+@app.route('/say/<name>')
+def say(name):
+    return "Hi "+name.capitalize()+"!"  #.capitalize()首字母大写
+
+@app.route('/repeat/<int:times>/<say_what>')
+def say_many_time(times,say_what):
+    say_return = ""
+    for i in range(times):
+        say_return += say_what +" "
+    return say_return
+@app.route('/<others>')
+def warning_system(others):
+    return "Sorry! No response. Try again."
 
 if __name__ == '__main__':
    app.run(debug = True)
