9530e7f28ff5ce2befd476ddbe86a73223938015
yudong <yoshino1347716570@gmail.com>
2019-10-11 02:21:56 +0800

    BONU update

--- django/django_intro/Ninja_Gold/apps/Ninja_Gold_app/urls.py
+++ django/django_intro/Ninja_Gold/apps/Ninja_Gold_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^process_money$', views.process_money),
     url(r'^destroy_session$', views.destroy_session),
+    url(r'^goal_input$', views.goal_setting),
     url(r'^$', views.index),
 ]
--- django/django_intro/Ninja_Gold/apps/Ninja_Gold_app/views.py
+++ django/django_intro/Ninja_Gold/apps/Ninja_Gold_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 import time
 import random
 id = 0
+
 def index(request):
     if 'player_gold' in request.session:
         pass
@@ -11,10 +12,22 @@ def index(request):
     if 'player_activities' in request.session:
         pass
     else:
-        request.session['player_activities'] = {}
+        request.session['player_activities'] = []
+
+    if 'goal_of_move' in request.session:
+        pass
+    else:
+        request.session['goal_of_move'] = 15
+    if 'goal_of_gold' in request.session:
+        pass
+    else:
+        request.session['goal_of_gold'] = 500
     context={
         'player_gold':request.session['player_gold'],
-        'player_activities':request.session['player_activities'],
+        'player_activities':list(reversed(request.session['player_activities'])),
+        'goal_of_gold':request.session['goal_of_gold'],
+        'goal_of_move':request.session['goal_of_move'],
+
     }
     return render(request,'Ninja_Gold_app/index.html',context)
 
@@ -31,34 +44,41 @@ def process_money(request):
     elif player_action == "casino":
         money_get = random.randint(-50, 50)
         #money_get = random.randint(150, 350)
-    if len(request.session['player_activities']) >= 15:
-        if request.session['player_gold'] < 500:
+    if len(request.session['player_activities']) >= request.session['goal_of_move']:
+        if request.session['player_gold'] < request.session['goal_of_gold']:
             request.session['player_gold']+=0
-            request.session['player_activities'][id]=["red","You failed! Try again.",""]
+            request.session['player_activities'].append(["red","You failed! Try again.",time_now])
             id+=1
         else:
             request.session['player_gold']+=0
-            request.session['player_activities'][id]=["green","You won!! Try again.",""]
+            request.session['player_activities'].append(["green","You won!! Try again.",time_now])
             id+=1
-    elif request.session['player_gold'] > 500:
+    elif request.session['player_gold'] > request.session['goal_of_gold']:
         request.session['player_gold']+=0
-        request.session['player_activities'][id]=["green","You won!! Try again.",""]
+        request.session['player_activities'].append(["green","You won!! Try again.",time_now])
         id+=1
     else:
         if money_get<0:
             request.session['player_gold']+=money_get
             what_happend = "Enter a casino and lost "+str(0-money_get)+" gold... Ouch..."
-            request.session['player_activities'][id]=["red",what_happend,time_now]
+            request.session['player_activities'].append(["red",what_happend,time_now])
             id+=1
         else:
             request.session['player_gold']+=money_get
             what_happend = "Earn "+str(money_get)+" gold from "+player_action+"!"
-            request.session['player_activities'][id]=["green",what_happend,time_now]
+            request.session['player_activities'].append(["green",what_happend,time_now])
             id+=1
     print(request.session['player_activities'])
     return redirect('/')
 
+def goal_setting(request):
+    request.session['goal_of_move'] = int(request.POST.get('goal_of_move'))
+    request.session['goal_of_gold'] = int(request.POST.get('goal_of_gold'))
+    return redirect('/')
+
 def destroy_session(request):
-    del request.session['player_gold'],
-    del request.session['player_activities'],
+    del request.session['player_gold']
+    del request.session['player_activities']
+    del request.session['goal_of_gold']
+    del request.session['goal_of_move']
     return redirect('/')
