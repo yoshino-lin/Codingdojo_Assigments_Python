3c1d7c7962c0694cebed30fccabbed23942bc712
yudong <yoshino1347716570@gmail.com>
2019-10-12 06:34:38 +0800

    new functions added

--- django/django_orm/Books_Authors_with_Templates/apps/Books_Authors_with_Templates_app/urls.py
+++ django/django_orm/Books_Authors_with_Templates/apps/Books_Authors_with_Templates_app/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^books/(?P<book_id>\d+)$', views.display_book),
+    url(r'^authors/(?P<author_id>\d+)$', views.display_author),
+    url(r'^book_author_add/(?P<book_id>\d+)$', views.add_author_to_book),
+    url(r'^author_book_add/(?P<author_id>\d+)$', views.add_book_to_author),
     url(r'^authors$', views.authors),
+    url(r'^book_add$', views.book_add),
+    url(r'^author_add$', views.author_add),
 ]
--- django/django_orm/Books_Authors_with_Templates/apps/Books_Authors_with_Templates_app/views.py
+++ django/django_orm/Books_Authors_with_Templates/apps/Books_Authors_with_Templates_app/views.py
@@ -13,6 +13,52 @@ def authors(request):
     }
     return render(request,'Books_Authors_with_Templates_app/authors.html',context)
 
+def book_add(request):
+    the_book_title = request.POST.get('title_add')
+    the_book_description = request.POST.get('description_add')
+    Book.objects.create(title=the_book_title,desc=the_book_description)
+    return redirect('/')
+
+def author_add(request):
+    the_author_first_name = request.POST.get('first_name_add')
+    the_author_last_name = request.POST.get('last_name_add')
+    the_author_note = request.POST.get('notes_add')
+    Author.objects.create(first_name = the_author_first_name, last_name = the_author_last_name, notes = the_author_note)
+    return redirect('/authors')
+
+def add_author_to_book(request,book_id):
+    this_book = Book.objects.get(id=book_id)
+    this_author_id = request.POST.get("author_id")
+    this_author = Author.objects.get(id=this_author_id)
+    this_book.author.add(this_author)
+    return redirect('/')
+
+def add_book_to_author(request,author_id):
+    this_author = Author.objects.get(id=author_id)
+    this_book_id = request.POST.get("book_id")
+    this_book = Book.objects.get(id=this_book_id)
+    this_book.author.add(this_author)
+    return redirect('/authors')
+
+def display_book(request,book_id):
+    this_book = Book.objects.get(id=book_id)
+    this_book_authors = this_book.author.all()
+    context = {
+        "this_book" : this_book,
+        "this_book_authors" : this_book_authors,
+        "authors_list":Author.objects.all(),
+    }
+    return render(request,'Books_Authors_with_Templates_app/books.html',context)
+
+def display_author(request,author_id):
+    this_author = Author.objects.get(id=author_id)
+    this_author_books = this_author.books.all()
+    context = {
+        "this_author" : this_author,
+        "this_author_books" : this_author_books,
+        "books_list":Book.objects.all(),
+    }
+    return render(request,'Books_Authors_with_Templates_app/author.html',context)
 
 def destroy_session(request):
     return redirect('/')
