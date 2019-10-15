975bbe0f4382c575a4d6e49fd5478d0af97fa843
yudong <yoshino1347716570@gmail.com>
2019-10-16 02:56:06 +0800

    bug fix

--- django/django_full_stack/Amadon/Amadon/urls.py
+++ django/django_full_stack/Amadon/Amadon/urls.py
@@ -13,9 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-
+from django.conf.urls import url, include	# added an import!
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('apps.Amadon_app.urls')),	# use your app_name here
 ]
--- django/django_full_stack/Amadon/apps/Amadon_app/views.py
+++ django/django_full_stack/Amadon/apps/Amadon_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from apps.Amadon_app.models import *
+"""
+Product.objects.create(name="Dojo T-shirt", price=19.99)
+Product.objects.create(name="Dojo Sweater", price=29.99)
+Product.objects.create(name="Dojo Cup", price=4.99)
+Product.objects.create(name="Algorithm Book", price=49.99)
+"""
+def index(request):
+    if 'total_amount_bought' in request.session:
+        pass
+    else:
+        request.session['total_amount_bought'] = 0
+    if 'total_monye_spent' in request.session:
+        pass
+    else:
+        request.session['total_monye_spent'] = 0.0
+    context = {
+        "product_list" : Product.objects.all(),
+    }
+    return render(request,'Amadon_app/index.html',context)
+def checkout(request):
+    quantity_of_item = int(request.POST.get('quantity'))
+    id_of_item = request.POST.get('item_id')
+    total_price = Product.objects.get(id=id_of_item).price * float(quantity_of_item)
+    request.session['total_amount_bought'] += quantity_of_item
+    request.session['total_monye_spent'] += total_price
+    context = {
+        "total_price" : total_price,
+        "amount_total" : request.session['total_amount_bought'],
+        "money_total" : request.session['total_monye_spent'],
+    }
+    return render(request,'Amadon_app/checkout.html',context)
