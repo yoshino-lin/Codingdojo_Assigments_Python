bb4acbf8a4a6a79d1bb0084e97b7b044bcbfde0d
yudong <yoshino1347716570@gmail.com>
2019-10-15 10:55:22 +0800

    update

--- django/django_full_stack/Semi_Restful_TV_Shows/apps/Semi_Restful_TV_Shows_app/models.py
+++ django/django_full_stack/Semi_Restful_TV_Shows/apps/Semi_Restful_TV_Shows_app/models.py
@@ -1,4 +1,35 @@
+from __future__ import unicode_literals
 from django.db import models
+import re
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
+
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        #检查字符长度
+        if len(postData["title_add"]) < 2:
+            errors["title"] = "Title should be at least 2 characters"
+        if len(postData["network_add"]) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+        if len(postData["description_add"]) != 0:
+            if len(postData["description_add"]) < 10:
+                errors["description"] = "Description should be at least 10 characters"
+        try:
+            get_item = Show.objects.get(title = postData["title_add"])
+            if get_item != None:
+                errors["Same title"] = "The title must be unqiue"
+        except ObjectDoesNotExist:
+            pass
+        if datetime.strptime(postData["release_date_add"], '%Y-%m-%d') > datetime.now():
+            errors["release_date"] = "Release Date cannot be in the future."
+        """
+        #检查邮箱地址是否非法
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern
+            errors['email'] = "Invalid email address!"
+        """
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,3 +38,4 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = BlogManager()
--- django/django_full_stack/Semi_Restful_TV_Shows/apps/Semi_Restful_TV_Shows_app/views.py
+++ django/django_full_stack/Semi_Restful_TV_Shows/apps/Semi_Restful_TV_Shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from apps.Semi_Restful_TV_Shows_app.models import *
 
 def index(request):
@@ -13,20 +14,38 @@ def show(request):
     }
     return render(request,'Semi_Restful_TV_Shows_app/show.html',context)
 def show_add(request):
-    the_title = request.POST.get('title_add')
-    the_network = request.POST.get('network_add')
-    the_release_date = request.POST.get('release_date_add')
-    the_description = request.POST.get('description_add')
-    Show.objects.create(title=the_title, network=the_network, release_date=the_release_date, desc = the_description)
-    new_id = Show.objects.last().id
-    return redirect('/shows/'+str(new_id))
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/shows/new')
+    else:
+        the_title = request.POST.get('title_add')
+        the_network = request.POST.get('network_add')
+        the_release_date = request.POST.get('release_date_add')
+        the_description = request.POST.get('description_add')
+        Show.objects.create(title=the_title, network=the_network, release_date=the_release_date, desc = the_description)
+        new_id = Show.objects.last().id
+        return redirect('/shows/'+str(new_id))
 def show_change(request,show_id):
-    the_show_to_update = Show.objects.get(id = show_id)
-    the_show_to_update.title = request.POST.get('title_add')
-    the_show_to_update.network = request.POST.get('network_add')
-    the_show_to_update.release_date = request.POST.get('release_date_add')
-    the_show_to_update.desc = request.POST.get('description_add')
-    the_show_to_update.save()
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect("/shows/"+str(show_id)+"/edit")
+    else:
+        the_show_to_update = Show.objects.get(id = show_id)
+        the_show_to_update.title = request.POST.get('title_add')
+        the_show_to_update.network = request.POST.get('network_add')
+        the_show_to_update.release_date = request.POST.get('release_date_add')
+        the_show_to_update.desc = request.POST.get('description_add')
+        the_show_to_update.save()
     return redirect('/shows/'+str(show_id))
 
 def show_display(request,show_id):
