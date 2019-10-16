876ebe7781d5b387f8a7079e904efd711306b282
yudong <yoshino1347716570@gmail.com>
2019-10-17 07:11:54 +0800

    finished

--- django/django_full_stack/The_Wall/apps/The_Wall_app/urls.py
+++ django/django_full_stack/The_Wall/apps/The_Wall_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^register$', views.registration),
+    url(r'^success$', views.login),
+    url(r'^logout$', views.logout),
+    url(r'^wall$', views.message_post),
+    url(r'^comment_post/(?P<message_id>\d+)$', views.comment_post),
+    url(r'^delete/(?P<message_id>\d+)$', views.delete_message),
 ]
--- django/django_full_stack/The_Wall/apps/The_Wall_app/views.py
+++ django/django_full_stack/The_Wall/apps/The_Wall_app/views.py
@@ -1,8 +1,72 @@
 from django.shortcuts import render, HttpResponse, redirect
 from apps.The_Wall_app.models import *
 from django.contrib import messages
+import time
+import datetime
 
 def index(request):
     context={
     }
-    return render(request,'Login_and_Registration_app/index.html',context)
+    return render(request,'The_Wall_app/login.html',context)
+
+def registration(request):
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+    else:
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        User.objects.create(first_name=request.POST.get('first_name'),last_name=request.POST.get('last_name'),email=request.POST.get('email'),password=pw_hash,birthday=request.POST.get('birthday'))
+    return redirect('/')
+
+def login(request):
+    if 'first_name' not in request.session:
+        errors = User.objects.basic_validator_2(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            user = User.objects.get(email=request.POST['email_login'])
+            request.session['user_id'] = user.id
+            request.session['first_name'] = user.first_name
+            request.session['last_name'] = user.last_name
+            context={
+                "username": request.session['first_name']+" "+request.session['last_name'],
+                "all_comment":Comment.objects.all(),
+                "all_message":Message.objects.all(),
+                "user_id":request.session['user_id'],
+            }
+            return render(request,'The_Wall_app/index.html',context)
+    else:
+        context={
+            "username": request.session['first_name']+" "+request.session['last_name'],
+            "all_comment":Comment.objects.all(),
+            "all_message":Message.objects.all(),
+            "user_id":request.session['user_id'],
+        }
+        return render(request,'The_Wall_app/index.html',context)
+def message_post(request):
+    the_user = User.objects.get(id=request.session['user_id'])
+    Message.objects.create(user=the_user,messages=request.POST['message_text'])
+    return redirect('/success')
+
+def comment_post(request,message_id):
+    the_user = User.objects.get(id=request.session['user_id'])
+    the_message = Message.objects.get(id=message_id)
+    Comment.objects.create(user=the_user,message=the_message,comment=request.POST['comment_text'])
+    return redirect('/success')
+
+def delete_message(request,message_id):
+    the_message = Message.objects.get(id=message_id)
+    time_dif = datetime.datetime.now() - the_message.created_at
+    if time_dif.seconds > 1800:
+        the_message.delete()
+    return redirect('/success')
+
+def logout(request):
+    del request.session['first_name']
+    del request.session['last_name']
+    del request.session['user_id']
+    return redirect('/')
