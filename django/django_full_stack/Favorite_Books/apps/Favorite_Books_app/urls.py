237f6a60dac2cb1d822c8871b4d20f315763005d
yudong <yoshino1347716570@gmail.com>
2019-10-18 05:37:02 +0800

    finished

--- django/django_full_stack/Favorite_Books/apps/Favorite_Books_app/urls.py
+++ django/django_full_stack/Favorite_Books/apps/Favorite_Books_app/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^books/(?P<book_id>\d+)$', views.book_edit),
+    url(r'^book_add$', views.book_add),
+    url(r'^register$', views.registration),
+    url(r'^success$', views.login),
+    url(r'^logout$', views.logout),
+    url(r'^update/(?P<book_id>\d+)$', views.book_update),
+    url(r'^delete_book/(?P<book_id>\d+)$', views.book_delete),
+    url(r'^unfavorite/(?P<book_id>\d+)$', views.unfavorite),
+    url(r'^addfavorite/(?P<book_id>\d+)$', views.addfavorite),
+    url(r'^book_display/(?P<book_id>\d+)$', views.book_display),
 ]
--- django/django_full_stack/Favorite_Books/apps/Favorite_Books_app/views.py
+++ django/django_full_stack/Favorite_Books/apps/Favorite_Books_app/views.py
@@ -7,7 +7,7 @@ import datetime
 def index(request):
     context={
     }
-    return render(request,'The_Wall_app/login.html',context)
+    return render(request,'Favorite_Books_app/login.html',context)
 
 def registration(request):
     errors = User.objects.basic_validator(request.POST)
@@ -34,35 +34,67 @@ def login(request):
             request.session['last_name'] = user.last_name
             context={
                 "username": request.session['first_name']+" "+request.session['last_name'],
-                "all_comment":Comment.objects.all(),
-                "all_message":Message.objects.all(),
                 "user_id":request.session['user_id'],
+                "all_books":Book.objects.all(),
+                "this_user":User.objects.get(id=request.session['user_id'])
             }
-            return render(request,'The_Wall_app/index.html',context)
+            return render(request,'Favorite_Books_app/index.html',context)
     else:
         context={
             "username": request.session['first_name']+" "+request.session['last_name'],
-            "all_comment":Comment.objects.all(),
-            "all_message":Message.objects.all(),
             "user_id":request.session['user_id'],
+            "all_books":Book.objects.all(),
+            "this_user":User.objects.get(id=request.session['user_id'])
         }
-        return render(request,'The_Wall_app/index.html',context)
-def message_post(request):
-    the_user = User.objects.get(id=request.session['user_id'])
-    Message.objects.create(user=the_user,messages=request.POST['message_text'])
+        return render(request,'Favorite_Books_app/index.html',context)
+
+def book_add(request):
+    this_use = User.objects.get(id=request.session['user_id'])
+    Book.objects.create(title=request.POST['title_of_the_book_to_add'],desc=request.POST["desc_of_the_book_to_add"],uploaded_by=this_use)
+    this_book = Book.objects.get(title=request.POST['title_of_the_book_to_add'])
+    this_book.users_who_like.add(this_use)
     return redirect('/success')
 
-def comment_post(request,message_id):
-    the_user = User.objects.get(id=request.session['user_id'])
-    the_message = Message.objects.get(id=message_id)
-    Comment.objects.create(user=the_user,message=the_message,comment=request.POST['comment_text'])
+def book_edit(request,book_id):
+    context={
+        "this_user":User.objects.get(id=request.session['user_id']),
+        "the_book":Book.objects.get(id=book_id),
+    }
+    return render(request,'Favorite_Books_app/edit.html',context)
+
+def book_update(request,book_id):
+    the_book = Book.objects.get(id=book_id)
+    the_book.title=request.POST['book_new_title']
+    the_book.desc=request.POST['book_new_desc']
+    the_book.save()
+    context={
+        "this_user":User.objects.get(id=request.session['user_id']),
+        "the_book":Book.objects.get(id=book_id),
+    }
+    return render(request,'Favorite_Books_app/edit.html',context)
+
+def book_delete(request,book_id):
+    c = Book.objects.get(id=book_id)
+    c.delete()
     return redirect('/success')
 
-def delete_message(request,message_id):
-    the_message = Message.objects.get(id=message_id)
-    time_dif = datetime.datetime.now() - the_message.created_at
-    if time_dif.seconds > 1800:
-        the_message.delete()
+def unfavorite(request,book_id):
+    the_user=User.objects.get(id=request.session['user_id'])
+    the_book=Book.objects.get(id=book_id)
+    the_user.liked_books.remove(the_book)
+    return redirect('/success')
+
+def book_display(request,book_id):
+    context={
+        "this_user":User.objects.get(id=request.session['user_id']),
+        "the_book":Book.objects.get(id=book_id),
+    }
+    return render(request,'Favorite_Books_app/info_display.html',context)
+
+def addfavorite(request,book_id):
+    the_user=User.objects.get(id=request.session['user_id'])
+    the_book=Book.objects.get(id=book_id)
+    the_user.liked_books.add(the_book)
     return redirect('/success')
 
 def logout(request):
